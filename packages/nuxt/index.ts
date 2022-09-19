cb6a974858bffb765b213fa3be41e68cc24b5b3d
chenying <1770365139@qq.com>
2022-09-19 15:26:44 +0800

    chore: bump Nuxt support to RC.10. use addImportsSources

--- packages/nuxt/index.ts
+++ packages/nuxt/index.ts
@@ -1,7 +1,7 @@
 import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
 import { isPackageExists } from 'local-pkg'
-import { defineNuxtModule } from '@nuxt/kit'
+import { addImportsSources, defineNuxtModule } from '@nuxt/kit'
 import { metadata } from '@vueuse/metadata'
 import type { Import } from 'unimport'
 
@@ -86,43 +86,39 @@ export default defineNuxtModule<VueUseNuxtOptions>({
 
     if (options.autoImports) {
       // auto import
-      nuxt.hook('imports:sources', (sources: any[]) => {
-        if (sources.find(i => fullPackages.includes(i.from)))
-          return
-
-        metadata.functions.forEach((i) => {
-          if (i.package === 'shared')
-            i.package = 'core'
-        })
-
-        for (const pkg of packages) {
-          if (pkg === 'shared')
-            continue
-
-          if (!isPackageExists(`@vueuse/${pkg}`))
-            continue
-
-          const imports = metadata
-            .functions
-            .filter(i => i.package === pkg && !i.internal)
-            .flatMap((i): Import[] => {
-              const names = [i.name, ...i.alias || []]
-              return names.map(n => ({
-                from: `@vueuse/${i.importPath || i.package}`,
-                name: n,
-                as: n,
-                priority: -1,
-              }))
-            })
-            .filter(i => i.name.length >= 4 && !disabledFunctions.includes(i.name))
-
-          sources.push({
-            from: '@vueuse/core',
-            imports,
-            priority: -1,
-          })
-        }
+      const presets = []
+      metadata.functions.forEach((i) => {
+        if (i.package === 'shared')
+          i.package = 'core'
       })
+      for (const pkg of packages) {
+        if (pkg === 'shared')
+          continue
+
+        if (!isPackageExists(`@vueuse/${pkg}`))
+          continue
+
+        const imports = metadata
+          .functions
+          .filter(i => i.package === pkg && !i.internal)
+          .flatMap((i): Import[] => {
+            const names = [i.name, ...i.alias || []]
+            return names.map(n => ({
+              from: `@vueuse/${i.importPath || i.package}`,
+              name: n,
+              as: n,
+              priority: -1,
+            }))
+          })
+          .filter(i => i.name.length >= 4 && !disabledFunctions.includes(i.name))
+
+        presets.push({
+          from: '@vueuse/core',
+          imports,
+          priority: -1,
+        })
+      }
+      addImportsSources(presets)
     }
   },
 })
